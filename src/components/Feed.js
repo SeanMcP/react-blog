718f06a33292058abbc78bf34fa38e920e6bdd14
Sean McPherson <seanmcp25@gmail.com>
2017-09-25 10:23:54 -0400

    Fetch blog posts on Feed component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-// import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Feed extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount() {
+    fetch('https://tiny-lasagna-server.herokuapp.com/collections/blogger/')
+    .then(results => results.json())
+    .then(data => this.setState({posts: data}))
+    .catch(err => console.log(err))
+  }
   render() {
+    let blogFeed = this.state.posts.map(post => {
+      return (
+        <li key={post._id}><Link to={`/view/${post._id}`}>{post.title}</Link> by {post.name}</li>
+      )
+    })
     return (
       <div>
-        <h1>The post list</h1>
+        <h1>Blog Feed</h1>
+        <ul>
+          {blogFeed}
+        </ul>
       </div>
     );
   }
--- src/components/View.js
+++ src/components/View.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { NavLink } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default class View extends Component {
   render() {
